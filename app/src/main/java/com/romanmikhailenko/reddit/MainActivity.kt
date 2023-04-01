05d1eef0ba3e32ed96300a2c0d04a937fab8fc56
alulcardino <52010205+alulcardino@users.noreply.github.com>
2023-04-01 13:30:38 +0600

    russian is added

--- app/src/main/java/com/romanmikhailenko/reddit/MainActivity.kt
+++ app/src/main/java/com/romanmikhailenko/reddit/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.romanmikhailenko.reddit
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.romanmikhailenko.reddit.databinding.ActivityMainBinding
 
@@ -17,6 +19,20 @@ class MainActivity : AppCompatActivity() {
                 binding.root.rootView,
             "Button is clicked", Snackbar.LENGTH_LONG).show()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getScreenOrientation()
+    }
+
+    private fun getScreenOrientation() {
+        val color = when(resources.configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> R.color.white
+            else -> R.color.black
+        }
+        binding.root.rootView.setBackgroundColor(getColor(color))
+
     }
 
 
